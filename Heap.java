4e088f7ad945554ad646c840a64dfc72c1fc254c
‘Gelila’ <79760415+Gelila21@users.noreply.github.com>
2022-04-19 13:16:00 -0700

    k

--- Heap.java
+++ Heap.java
@@ -1,1 +1,55 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io. IOException;
+public class Heap{
 
+private int size;
+private int[] arr;
+public Heap(){}
+//buidHeap method that works in O(n) time
+public void buildHeap(int[] ar) {
+this.size = ar.length;
+this.arr = new int[size];
+for(int i=0;i<ar.length;i++)
+this.arr[i] = ar[i];
+for (int i=(this.arr.length) /2;i>=0;i--){
+this.heapify(i);}
+}
+//max-heapify method
+public void heapify(int i){
+int largest = i;
+if (((2*i) +1)<size && this.arr [(2*i)+1]>this.arr[largest])
+largest = ((2*i)+1);
+
+if (((2*i) +2)<size && this.arr [(2*i)+1]>this.arr[largest])
+largest = ((2*i) +2) ;
+if (largest != i){
+int temp = this.arr[i];
+this.arr[i] = this.arr[largest];
+this.arr[largest] = temp;
+this.heapify(largest);
+}}
+public void insert(int num) {}
+//method to remove max element from max-heap
+public int remove() {
+if (this.size<1)
+return -1;
+int temp = this.arr[0];
+this.arr[0] = this.arr[size-1];
+this.arr[size-1] = temp;
+size--;
+return this.arr[size];}
+//method to print elements of max heap in output file
+public void printHeap (String filename){
+try{
+File file = new File(filename);
+FileWriter writer = new FileWriter (file);
+for (int i = 0; i
+< this.arr.length; i++)
+writer.write(this.arr[i] + "(n") ;
+writer.close();
+}catch (IOException e){
+System.out.println ("Error while writing");
+}
+}
+}
--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Main{
+public static void main(String[] args){
+try{
+ArrayList<Integer> readArray = new ArrayList<Integer>() ;
+File file = new File("/Users/gelilachaka/downloads/data_random.txt");
+Scanner reader = new Scanner(file);
+while (reader.hasNextInt()){
+readArray.add(reader.nextInt());
+}
+int[] arr = new int[readArray.size()];
+for (int i =0; i< readArray.size(); i++)
+arr[i] = readArray.get(i);
+Heap h = new Heap();
+h.buildHeap(arr);
+h.printHeap ( "output.txt");
+}catch (IOException e){
+System.out.println("Error while reading") ;
+}
+}
+}
